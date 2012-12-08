e905c44f6a1202ef60d020bf068111c44a1d9600
Adam Honoré <Adam.Honore@Gmail.com>
2012-12-08 14:16:52 +0100

    tests are back!

---
+++ src/test/java/DemoProgram.java
@@ -0,0 +1,45 @@
+import spreadsheet.CellReference;
+import spreadsheet.Expression;
+import spreadsheet.Position;
+import spreadsheet.Spreadsheet;
+import spreadsheet.arithmetic.AConst;
+import spreadsheet.arithmetic.Add;
+import spreadsheet.logical.Conjunct;
+import spreadsheet.logical.Disjunct;
+import spreadsheet.logical.LConst;
+import spreadsheet.textual.Concat;
+import spreadsheet.textual.TConst;
+
+public class DemoProgram {
+
+    public static void main(String[] args) {
+
+        final Spreadsheet spreadsheet = new Spreadsheet();
+
+        // Create an expression for the first cell
+        Expression e1 = new Conjunct(new LConst(true), new TConst(""));
+
+        Expression e2 = new Disjunct(new LConst(false), new TConst("true"));
+
+        Expression efoo = new Add(e1, e2);
+        spreadsheet.set(new Position(1, 1), efoo);
+
+        // Create an expression for the second cell
+        Expression e3 = new CellReference(spreadsheet, new Position(1, 1));
+
+        Expression e4 = new AConst(41);
+        Expression ebar = new Add(e3, e4);
+
+        spreadsheet.set(new Position(2, 1), ebar);
+        Expression e5 = new TConst("The answer to life, the universe and everything: ");
+
+        Expression e6 = new CellReference(spreadsheet, new Position(2, 1));
+
+        Expression ebaz = new Concat(e5, e6);
+        spreadsheet.set(new Position(4, 4), ebaz);
+
+        // Extract values from the spreadsheet
+        String result = spreadsheet.get(new Position(4, 4)).toString();
+        System.out.println(result);
+    }
+}
