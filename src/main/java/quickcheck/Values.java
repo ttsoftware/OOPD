e090576aca1d5986228884bdd717d85b94eee6b7
Troels Thomsen <tt.ttsoftware@gmail.com>
2012-12-10 15:13:08 +0100

    GENERATORS

--- src/main/java/quickcheck/Values.java
+++ src/main/java/quickcheck/Values.java
@@ -8,14 +8,13 @@ public class Values<T> implements Iterable<T> {
     private final ArrayList<T> list = new ArrayList<T>(10);
 
     public Values(Generator<T> generator) {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++) {
             list.add(generator.next());
+        }
     }
 
     @Override
     public Iterator<T> iterator() {
         return list.iterator();
     }
-
-
 }
---
+++ src/main/java/spreadsheet/ExpressionGenerator.java
@@ -0,0 +1,11 @@
+package spreadsheet;
+
+import quickcheck.Generator;
+
+public class ExpressionGenerator extends Generator<Expression> {
+
+    @Override
+    public Expression next() {
+        return null;
+    }
+}
