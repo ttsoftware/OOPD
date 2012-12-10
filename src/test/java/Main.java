1cfdb2008d807165d0ec35d36334633f153fa93a
Adam Honoré <Adam.Honore@Gmail.com>
2012-12-10 14:16:31 +0100

    stuff has been done to stuff!

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     @Test
     public void testSpreadsheet() {
-
-        // Vi kører bare tests sådan her fremover, er det nemmeste ;-)
+        DIKUTest.main(null);
     }
 }
