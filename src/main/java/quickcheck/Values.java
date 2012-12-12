5dac3217cf8abae86e844b2c0338a75e38237759
Adam Honoré <Adam.Honore@Gmail.com>
2012-12-12 13:11:30 +0100

    rearranged :P

--- src/main/java/quickcheck/Values.java
+++ src/main/java/quickcheck/Values.java
@@ -7,14 +7,14 @@ public class Values<T> implements Iterable<T> {
 
     private final ArrayList<T> list = new ArrayList<T>(10);
 
+    public Values(Generator<T> generator) {
+        this(generator, 10);
+    }
+
     public Values(Generator<T> generator, Integer count) {
         for (int i = 0; i < count; i++) {
             list.add(generator.next());
         }
-    }
-
-    public Values(Generator<T> generator) {
-        this(generator, 10);
     }
 
     @Override
