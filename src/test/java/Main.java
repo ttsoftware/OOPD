dda4cdf9f9e55962de9865069b66d81dbac0a1ee
Adam Honoré <Adam.Honore@Gmail.com>
2012-12-13 13:17:34 +0100

    removed old tests from main testing class to make output cleaner

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,6 +1,4 @@
 import org.junit.runner.JUnitCore;
-import tests.A2;
-import tests.A3;
 import tests.ExpressionTest;
 import tests.PositionTest;
 
@@ -8,10 +6,6 @@ public class Main {
 
     @org.junit.Test
     public void test() {
-        JUnitCore.runClasses(
-                A2.class,
-                A3.class,
-                PositionTest.class,
-                ExpressionTest.class);
+        JUnitCore.runClasses(PositionTest.class, ExpressionTest.class);
     }
 }
